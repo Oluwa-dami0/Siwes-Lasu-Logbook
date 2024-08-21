8e1048859d49af83b5a4ae289c2d2d09a6e42c67
Oluwa-dami0 <shekereabideen10@gmail.com>
2024-08-21 18:29:04 +0100

    supervisor_login

--- login.php
+++ login.php
@@ -41,21 +41,6 @@
     <title>Siwes Logbook Login</title>
 </head>
 <body>
-<!-- <div class="login-container supervisor-login">
-        <h2> Supervisor Login</h2>
-        <form id="supervisor-login">
-            <div class="form-group">
-                <label for="pf-number">PF-Number</label>
-                <input type="number" id="pf-number" name="PF-number" required>
-            </div>
-            <div class="form-group">
-                <label for="sp-password">Password</label>
-                <input type="password" id="sp-password" name="sp-password" required>
-            </div>
-            <a href="" class="forget-password"><p>Forgot password?</p></a>
-            <button type="submit" onclick="goToPage2()">Login</button>
-        </form>
-    </div>  -->
 <div class="login-container student-login">
         <h2>Student Login</h2>
         <?php echo (!empty($login_err)) ? 
---
+++ supervisor_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supervisor Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="login-container supervisor-login">
+        <h2> Supervisor Login</h2>
+        <form id="supervisor-login">
+            <div class="form-group">
+                <label for="pf-number">PF-Number</label>
+                <input type="number" id="pf-number" name="PF-number" required>
+            </div>
+            <div class="form-group">
+                <label for="sp-password">Password</label>
+                <input type="password" id="sp-password" name="sp-password" required>
+            </div>
+            <a href="" class="forget-password"><p>Forgot password?</p></a>
+            <button type="submit" onclick="goToPage2()">Login</button>
+        </form>
+    </div> 
+   
+</body>
+</html>
